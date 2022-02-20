26d3fdf34627cc2068988990c90d4000fb6a9ee0
SmokingRat <halfbrikbarry@gmail.com>
2022-02-20 12:54:10 +0300

    Создание

---
+++ Module4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Module4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите A");
+            string A = Console.ReadLine();
+            Console.WriteLine("Введите В");
+            string B = Console.ReadLine();
+            var Result = A != B;
+            Console.WriteLine(Result);
+            Console.ReadKey();
+        }
+    }
+}
