b510811c84784353973472cd0541674f13d0f681
SmokingRat <halfbrikbarry@gmail.com>
2022-02-20 14:29:10 +0300

    Unit 4.1

--- Module4/Program.cs
+++ Module4/Program.cs
@@ -6,13 +6,33 @@ namespace Module4
     {
         static void Main(string[] args)
         {
-            int A = 432;
-            int B = 434;
-            double X = 3.4444;
-            double Y = 3.5;
-            var Result = (A < B) || (X > Y);
-            Console.WriteLine(Result);
-            Console.ReadKey();
+			Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+			var color = Console.ReadLine();
+
+			if (color == "red")
+			{
+				Console.BackgroundColor = ConsoleColor.Red;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is red!");
+			}
+
+			else if (color == "green")
+			{
+				Console.BackgroundColor = ConsoleColor.Green;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is green!");
+			}
+			else
+			{
+				Console.BackgroundColor = ConsoleColor.Cyan;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is cyan!");
+			}
+				Console.ReadKey();
         }
     }
 }
