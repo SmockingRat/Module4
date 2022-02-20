a965a240bf9141f0cfede8f5befad255d7dd3c07
SmokingRat <halfbrikbarry@gmail.com>
2022-02-20 14:34:29 +0300

    Оператор switch

--- Module4/Program.cs
+++ Module4/Program.cs
@@ -10,29 +10,37 @@ namespace Module4
 
 			var color = Console.ReadLine();
 
-			if (color == "red")
+			switch (color)
 			{
-				Console.BackgroundColor = ConsoleColor.Red;
-				Console.ForegroundColor = ConsoleColor.Black;
+				case "red":
+					Console.BackgroundColor = ConsoleColor.Red;
+					Console.ForegroundColor = ConsoleColor.Black;
 
-				Console.WriteLine("Your color is red!");
+					Console.WriteLine("Your color is red!");
+					break;
+
+				case "green":
+					Console.BackgroundColor = ConsoleColor.Green;
+					Console.ForegroundColor = ConsoleColor.Black;
+
+					Console.WriteLine("Your color is green!");
+					break;
+
+				case "cyan":
+					Console.BackgroundColor = ConsoleColor.Cyan;
+					Console.ForegroundColor = ConsoleColor.Black;
+
+					Console.WriteLine("Your color is cyan!");
+					break;
+
+				default:
+					Console.BackgroundColor = ConsoleColor.Yellow;
+					Console.ForegroundColor = ConsoleColor.Red;
+
+					Console.WriteLine("Your didn't choose color!");
+					break;
 			}
-
-			else if (color == "green")
-			{
-				Console.BackgroundColor = ConsoleColor.Green;
-				Console.ForegroundColor = ConsoleColor.Black;
-
-				Console.WriteLine("Your color is green!");
-			}
-			else
-			{
-				Console.BackgroundColor = ConsoleColor.Cyan;
-				Console.ForegroundColor = ConsoleColor.Black;
-
-				Console.WriteLine("Your color is cyan!");
-			}
-				Console.ReadKey();
+			Console.ReadKey();
         }
     }
 }
