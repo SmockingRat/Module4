10d5f37288085bd12e3c1d0724887def9054366a
SmokingRat <halfbrikbarry@gmail.com>
2022-02-23 00:49:27 +0300

    Сложение элементов массива

--- Module4/Program.cs
+++ Module4/Program.cs
@@ -10,25 +10,16 @@ namespace Module4
             int[] Arr = new int[] {800, 1, 34, 45, 55, 4, 56, 7, 3, 99, 312};
 
         
-            int Accum = 0;
+            int Accum=0;
+            
 
             for(int i = 0; i < Arr.Length; i++)
             {
-                for(int j = 0; j < Arr.Length - 1; j++)
-                {
-                    if (Arr[j] > Arr[j+1])
-                    {
-                        Accum = Arr[j+1];
-                        Arr[j+1] = Arr[j];
-                        Arr[j] = Accum;
-                    }
-                }
+                
+                Accum += Arr[i];
             }
 
-            foreach(int Num in Arr)
-            {
-                Console.Write(Num + " ");
-            }
+            Console.WriteLine(Accum);
 
             Console.ReadKey();
         }
