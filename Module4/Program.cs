f5a653456aa4a2407b5f98798c664a1804761d27
SmokingRat <halfbrikbarry@gmail.com>
2022-02-22 23:06:24 +0300

    Массив с имнем

--- Module4/Program.cs
+++ Module4/Program.cs
@@ -6,58 +6,19 @@ namespace Module4
     {
         static void Main(string[] args)
         {
-			string[] favcolor = new string[6];
+            Console.WriteLine("Введите своё имя");
 
-            for(int i=0; i<favcolor.Length; i++ )
+            string YourName = Console.ReadLine();
+
+            Console.WriteLine("Ваше имя по символам");
+            foreach(var ch in YourName)
             {
-                Console.WriteLine("Введите свой любимый цвет {0}", i+1);
-                favcolor[i] = Console.ReadLine();
-            }
-
-            foreach (var color in favcolor)
-            {
-                switch(color)
-                {
-
-                    case "cyan":
-                        Console.BackgroundColor = ConsoleColor.Cyan;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is cyan");
-                        break;
-
-                    case "green":
-                        Console.BackgroundColor = ConsoleColor.Green;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is green");
-                        break;
-
-                    case "red":
-                        Console.BackgroundColor = ConsoleColor.Red;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is red");
-                        break;
-
-                    case "yellow":
-                        Console.BackgroundColor = ConsoleColor.Yellow;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is yellow");
-                        break;
-
-                    default:
-                        Console.BackgroundColor = ConsoleColor.White;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is white");
-
-                        break;
+                Console.Write(ch + " ");
                 
-                
-                }
             }
+            Console.WriteLine();
+
+            Console.WriteLine("Последняя буква вашего имени \n{0}", YourName[YourName.Length - 1 ]);
 
 			Console.ReadKey();
         }
