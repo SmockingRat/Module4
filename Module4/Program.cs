d0be4132c619b1a9447722273ad7f4cb45a3af23
SmokingRat <halfbrikbarry@gmail.com>
2022-02-23 00:17:56 +0300

    Перебор массива по стобцам

--- Module4/Program.cs
+++ Module4/Program.cs
@@ -6,20 +6,24 @@ namespace Module4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите своё имя");
 
-            string YourName = Console.ReadLine();
+            int[,] DArr = {{1,4,3,54,55,5},{23,55,5,5,66,45},{34,55,5,3,2,4} };
 
-            Console.WriteLine("Ваше имя по символам");
-            for(int i = YourName.Length - 1; i >= 0; i--)
+            for(int i =0; i < DArr.GetUpperBound(1)+1; i++)
             {
-                Console.Write(YourName[i] + " ");
-                
+                for(int j = 0; j < DArr.GetUpperBound(0)+1; j++)
+                {
+                    Console.Write(DArr[j,i] + " ");
+                }
+                Console.WriteLine();
             }
-            Console.WriteLine();
 
 
-			Console.ReadKey();
+            Console.WriteLine("Кол-во строк");
+            Console.WriteLine(DArr.GetUpperBound(0)+1+" ");
+            Console.WriteLine("Кол-во колонок");
+            Console.WriteLine(DArr.GetUpperBound(1)+1+" ");
+            Console.ReadKey();
         }
     }
 }
