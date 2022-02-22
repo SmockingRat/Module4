15b97c91c82ce868aa8d7dff109b544525cb5445
SmokingRat <halfbrikbarry@gmail.com>
2022-02-23 01:22:32 +0300

    Подсчёт положительных элементов в двумерном массиве

--- Module4/Program.cs
+++ Module4/Program.cs
@@ -7,18 +7,20 @@ namespace Module4
         static void Main(string[] args)
         {
 
-            int[] Arr = new int[] {12, 33, 54, -34, -6, 6, -45, -65, -1, 0, -55 };
+            int[,] Arr = { { 12, 33, 54, -34, -6, 6, -45, -65, -1, 0, -55 }, {34, -65, -45, 34, -44, 2, -5, 6, -3, 7, 4 } };
 
             int Accum = 0;
 
-            for(int i = 0; i < Arr.Length; i++)
+            for (int j = 0; j < Arr.GetUpperBound(0)+1; j++)
             {
-                if (Arr[i] > 0)
+                for (int i = 0; i < Arr.GetUpperBound(1) + 1; i++)
                 {
-                    Accum += 1;
+                    if (Arr[j, i] > 0)
+                    {
+                        Accum += 1;
+                    }
                 }
             }
-
             Console.WriteLine(Accum);
 
             Console.ReadKey();
