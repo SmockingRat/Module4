e29bdb7f02c59664887db83904cc31d7d24c4b48
SmokingRat <halfbrikbarry@gmail.com>
2022-02-23 22:42:23 +0300

    Ввод данных со стороны пользователя

--- Module4/Program.cs
+++ Module4/Program.cs
@@ -7,11 +7,20 @@ namespace Module4
         static void Main(string[] args)
         {
 
-            var anketa = (Name: "George", Age: 19, Pet: "Dog and Bird" );
+            (int Age, string Name, string Pet) Anketa;
 
-            Console.WriteLine(anketa.Name);
-            Console.WriteLine(anketa.Age);
-            Console.WriteLine(anketa.Pet);
+            Console.WriteLine("Введите ваше имя");
+            Anketa.Name = Console.ReadLine();
+
+            Console.WriteLine("Введите ваш возраст");
+            Anketa.Age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Какие у вас есть питомцы?");
+            Anketa.Pet = Console.ReadLine();
+
+            Console.WriteLine("Ваше имя {0}", Anketa.Name);
+            Console.WriteLine("Ваш возраст {0}", Anketa.Age);
+            Console.WriteLine("Ваши питомцы {0}", Anketa.Pet);
 
             Console.ReadKey();
         }
