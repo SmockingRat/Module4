feb3b20c8dc1ddbf798060e45144dfa7750ca06b
SmokingRat <halfbrikbarry@gmail.com>
2022-02-23 23:06:41 +0300

    Анкета питомца

--- Module4/Program.cs
+++ Module4/Program.cs
@@ -7,20 +7,30 @@ namespace Module4
         static void Main(string[] args)
         {
 
-            (int Age, string Name, string Pet) Anketa;
+            (double Age, string Name, string Type, int NameCount) Pet;
 
-            Console.WriteLine("Введите ваше имя");
-            Anketa.Name = Console.ReadLine();
+            Console.WriteLine("Введите имя питомца");
+            Pet.Name = Console.ReadLine();
 
-            Console.WriteLine("Введите ваш возраст");
-            Anketa.Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите возраст питомца");
+            Pet.Age = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Какие у вас есть питомцы?");
-            Anketa.Pet = Console.ReadLine();
+            Console.WriteLine("Каким животным является ваш питомец");
+            Pet.Type = Console.ReadLine();
 
-            Console.WriteLine("Ваше имя {0}", Anketa.Name);
-            Console.WriteLine("Ваш возраст {0}", Anketa.Age);
-            Console.WriteLine("Ваши питомцы {0}", Anketa.Pet);
+            Pet.NameCount = 0;
+
+            for (int i = 0; i < Pet.Name.Length; i++)
+            {
+                Pet.NameCount++;
+            }
+
+            Console.WriteLine("Имя питомца {0}", Pet.Name);
+            Console.WriteLine("Возраст питомца {0}", Pet.Age);
+            Console.WriteLine("Ваш питомец {0}", Pet.Type);
+            Console.WriteLine("Длина имени вашего питомца {0}", Pet.NameCount);
+
+
 
             Console.ReadKey();
         }
